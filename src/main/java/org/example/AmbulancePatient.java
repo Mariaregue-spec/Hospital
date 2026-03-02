5f32e3c22a4e145900ba7b6c556a8953d751730a
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-02 17:46:54 +0100

    Feat AmbulancePatient.java

--- src/main/java/org/example/AmbulancePatient.java
+++ src/main/java/org/example/AmbulancePatient.java
@@ -1,4 +1,50 @@
 package org.example;
 
-public class AmbulancePatient {
+public class AmbulancePatient extends Patient
+        implements CommonPatient, AmbulanceActions {
+
+    private String emergency;
+    private boolean surgeryRequired;
+
+    public AmbulancePatient(String name, String surname, String medicalRecordNumber,
+                            String emergency, boolean surgeryRequired) {
+        super(name, surname, medicalRecordNumber);
+        this.emergency = emergency;
+        this.surgeryRequired = surgeryRequired;
+    }
+
+    @Override
+    public void admission() {
+        System.out.println("Ingreso de emergencia.");
+    }
+
+    @Override
+    public void operation() {
+        System.out.println("Operación en curso.");
+    }
+
+    @Override
+    public void room() {
+        System.out.println("Paciente asignado a UCI.");
+    }
+
+    @Override
+    public void treatment() {
+        System.out.println("Se aplicó tratamiento de emergencia.");
+    }
+
+    @Override
+    public void rehabilitation() {
+        System.out.println("Comenzó la rehabilitación.");
+    }
+
+    @Override
+    public void discharge() {
+        System.out.println("Paciente dado de alta.");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Paciente de ambulancia: " + getName() + " " + getSurname());
+    }
 }
