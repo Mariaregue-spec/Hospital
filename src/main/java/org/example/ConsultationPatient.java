9bd2090e0783785658dbd53109ef95ad92d3c0a3
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-02 17:20:04 +0100

    Feat ConsultationPatient.java

--- src/main/java/org/example/ConsultationPatient.java
+++ src/main/java/org/example/ConsultationPatient.java
@@ -1,4 +1,47 @@
 package org.example;
 
-public class ConsultationPatient extends Patient {
+public class ConsultationPatient extends Patient implements CommonPatient, ConsultationActions {
+    private String appointmentDate;
+    private String nurseInCharge;
+
+    public ConsultationPatient(String name, String surname, String medicalRecordNumber,String appointmentDate, String nurseInCharge) {
+        super(name, surname, medicalRecordNumber);
+        this.appointmentDate = appointmentDate;
+        this.nurseInCharge = nurseInCharge;
+    }
+
+    @Override
+            System.out.println("Paciente admitido para consulta.") {
+
+    }
+
+    @Override
+            System.out.println("Chequeo de enfermero/a completado.") {
+
+    }
+
+    @Override
+            System.out.println("Paciente asignado a habitación.") {
+
+    }
+
+    @Override
+            System.out.println("Tratamiento en curso.") {
+
+    }
+
+    @Override
+            System.out.println("Próxima cita programada.") {
+
+    }
+
+    @Override
+        System.out.pirntln("Paciente dado de alta.") {
+
+    }
+
+    @Override
+            System.out.println("Consultation Patient: " + getName() + " " + getSurname());
+
+    }
 }
