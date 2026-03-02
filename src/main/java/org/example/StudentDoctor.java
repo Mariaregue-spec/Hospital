2b3a52ab485980ca26d77e67e56a884ff77300c3
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-02 18:51:43 +0100

    Feat StudentDoctor.java

--- src/main/java/org/example/StudentDoctor.java
+++ src/main/java/org/example/StudentDoctor.java
@@ -1,4 +1,39 @@
 package org.example;
 
-public class StudentDoctor {
+public class StudentDoctor extends MedicalStaff
+        implements DoctorCommon, StudentInfo {
+
+    private String university;
+
+    public StudentDoctor(String name, String surname, String role,
+                         String university) {
+        super(name, surname, role);
+        this.university = university;
+    }
+
+    @Override
+    public void weeklyWorkingHours() {
+        System.out.println("Trabaja 30 horas a la semana.");
+    }
+
+    @Override
+    public void weeklyStudyHours() {
+        System.out.println("Estudia 20 horas a la semana.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("El estudiante está comiendo.");
+
+    }
+
+    @Override
+    public void currentCourse() {
+        System.out.println("Actualmente en rotación de Cirugía.");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Student Doctor: " + getName());
+    }
 }
