dc2a9544a35e0b8aa55bec8714cbccdd2ab7786b
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-02 18:24:31 +0100

    Feat SpecialistDoctor.java

--- src/main/java/org/example/SpecialistDoctor.java
+++ src/main/java/org/example/SpecialistDoctor.java
@@ -1,4 +1,36 @@
 package org.example;
 
-public class SpecialistDoctor {
+public class SpecialistDoctor extends MedicalStaff implements DoctorCommon, MedicalInfo {
+    private String licenseNumber;
+
+    public SpecialistDoctor(String name, String surname, String role, String licenseNumber) {
+        super(name, surname, role);
+        this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public void showInfo(){
+        System.out.println("Doctor Especialista: " + getName());
+    }
+
+    @Override
+    public void weeklyWorkingHours(){
+        System.out.println("Trabaja 40 horas por semana.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("El Doctor está comiendo.");
+    }
+
+    @Override
+    public void vacationDays() {
+        System.out.println("Tiene 30 días de vacaciones.");
+    }
+
+    @Override
+    public void specialty() {
+        System.out.println("Doctor Especialista en Cardiología");
+    }
 }
+
