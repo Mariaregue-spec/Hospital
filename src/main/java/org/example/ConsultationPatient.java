d31fe1151c7cff5022fbbe010f0b5b28a2fbc58a
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-02 17:40:52 +0100

    Feat ConsultationPatient.java

--- src/main/java/org/example/ConsultationPatient.java
+++ src/main/java/org/example/ConsultationPatient.java
@@ -4,44 +4,51 @@ public class ConsultationPatient extends Patient implements CommonPatient, Consu
     private String appointmentDate;
     private String nurseInCharge;
 
-    public ConsultationPatient(String name, String surname, String medicalRecordNumber,String appointmentDate, String nurseInCharge) {
+    public ConsultationPatient(String name, String surname, String medicalRecordNumber, String appointmentDate, String nurseInCharge) {
         super(name, surname, medicalRecordNumber);
         this.appointmentDate = appointmentDate;
         this.nurseInCharge = nurseInCharge;
     }
 
     @Override
-            System.out.println("Paciente admitido para consulta.") {
+    public void admission() {
+        System.out.println("Paciente admitido para consulta.");
 
     }
 
     @Override
-            System.out.println("Chequeo de enfermero/a completado.") {
+    public void nurseCheckUp() {
+        System.out.println("Chequeo de enfermero/a completado.");
 
     }
 
     @Override
-            System.out.println("Paciente asignado a habitación.") {
+    public void room() {
+        System.out.println("Paciente asignado a habitación.");
 
     }
 
     @Override
-            System.out.println("Tratamiento en curso.") {
+    public void treatment() {
+        System.out.println("Tratamiento en curso.");
 
     }
 
     @Override
-            System.out.println("Próxima cita programada.") {
+    public void nextAppointment() {
+        System.out.println("Próxima cita programada.");
 
     }
 
     @Override
-        System.out.pirntln("Paciente dado de alta.") {
+    public void discharge() {
+        System.out.println("Paciente dado de alta.");
 
     }
 
     @Override
-            System.out.println("Consultation Patient: " + getName() + " " + getSurname());
-
+    public void showInfo() {
+        System.out.println("Consultation Patient: " + getName() + " " + getSurname());
     }
 }
+
